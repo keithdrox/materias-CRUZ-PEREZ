b8613350e31cd9ad53e9ea43613cb8030567ad27
keithdrox <justyncruzperez@gmail.com>
2026-07-03 09:53:00 -0500

    Add schema and seed SQL files for usuarios and materias tables

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+-- Hash placeholder. It will be replaced.
+INSERT INTO usuarios (username, password_hash, rol) VALUES 
+('admin', '$2y$10$XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 'ADMIN');
